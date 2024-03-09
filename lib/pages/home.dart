ecf02dbf7600dbe3bc68fdfe50d6b81708aa2bb6
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-03-09 14:04:46 +0530

    Add content to home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,7 +9,44 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Employzer"),
       ),
-      body: const Text("Home"),
+      body: Column(
+        children: [
+          Text("SCA ${user.email}"),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('About me'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Work experience'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Education'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Skill'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Qualification'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
